3598bbcd779bdd98160d6ffb2c027cb4317520a4
Hao-Quan Liu <haoquan291@gmail.com>
2025-07-28 15:40:21 +0800

    refactor: use importlib to import tools module to avoid import warnings

--- src/protocols_io_mcp/server.py
+++ src/protocols_io_mcp/server.py
@@ -1,8 +1,8 @@
+import importlib
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP(
     name="protocols-io-mcp-server",
     instructions="""This server provides access to protocols.io API."""
 )
-
-import protocols_io_mcp.tools
+importlib.import_module('protocols_io_mcp.tools')
